9e80b2afc3780067e44af184d196a405d701b305
Saitek2805 <145493957+Saitek2805@users.noreply.github.com>
2024-10-09 21:42:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Establecer el tipo de contenido a texto plano
+header("Content-Type: text/plain");
+
+// Mensaje de bienvenida
+echo "¡Hola, Mundo!\n";
+
+// Obtener la fecha y hora actual
+echo "La fecha y hora actual es: " . date("d/m/Y H:i:s") . "\n";
+
+// Mensaje adicional
+echo "Esta es una página simple generada con PHP.";
+?>
